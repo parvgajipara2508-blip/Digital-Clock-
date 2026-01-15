a0280f924188532fa99c23972112fe61f8a4a74a
parv gajipara <parvgajipara2508@gmail.com>
2026-01-15 16:39:58 +0530

    adding code

---
+++ app.js
@@ -0,0 +1,22 @@
+const time = document.querySelector('.time');
+const timeformate = document.querySelector('.time-formate');
+
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(showTime, 1000);
+});
+
+const showTime = () => {
+    let date = new Date();
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hr = hr < 10 ? `0${hr}` : hr;
+    min = min < 10 ? `0${min}` : min;
+    sec = sec < 10 ? `0${sec}` : sec;
+
+    timeformate.innerHTML = hr > 12 ? "PM" : "AM";
+
+    time.innerHTML = `${hr} : ${min} : ${sec}`;
+}
